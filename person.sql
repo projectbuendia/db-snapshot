01985d1370b74e8182bc5c43a180931a5e51acc2
Buendia - Jenkins <buendiajenkins@gmail.com>
2015-08-27 22:02:16 +0000

    Snapshot 2663

--- person.sql
+++ person.sql
@@ -49,7 +49,7 @@ CREATE TABLE `person` (
   CONSTRAINT `user_who_changed_person` FOREIGN KEY (`changed_by`) REFERENCES `users` (`user_id`),
   CONSTRAINT `user_who_created_person` FOREIGN KEY (`creator`) REFERENCES `users` (`user_id`),
   CONSTRAINT `user_who_voided_person` FOREIGN KEY (`voided_by`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1868 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1869 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
